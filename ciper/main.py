bd760e8da66ea02e21c913482c201b0eddfec98e
santhiya21 <santhiya.ganesh.13@gmail.com>
2023-05-01 11:03:43 -0500

    updating

--- ciper/main.py
+++ ciper/main.py
@@ -7,15 +7,15 @@ import encryptMessage
 def runCaesarCipherProgram():
     myAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     print(f'Alphabet: {myAlphabet}')
-    myAlphabet2 = getDoubleAlphabet(myAlphabet)
+    myAlphabet2 = getDoubleAlphabet.getDoubleAlaphabet(myAlphabet)
     print(f'Alphabet2: {myAlphabet2}')
-    myMessage = getmessage()
+    myMessage = getmessage.getmessage()
     print(myMessage)
-    myCipherKey = getCipherKey()
+    myCipherKey = getCipherKey.getCipherKey()
     print(myCipherKey)
-    myEncryptedMessage = encryptMessage(myMessage, myCipherKey, myAlphabet2)
+    myEncryptedMessage = encryptMessage.encryptMessage(myMessage, myCipherKey, myAlphabet2)
     print(f'Encrypted Message: {myEncryptedMessage}')
     myDecryptedMessage = decryptMessage(myEncryptedMessage, myCipherKey, myAlphabet2)
     print(f'Decypted Message: {myDecryptedMessage}')
     
-    runCaesarCipherProgram()
+runCaesarCipherProgram()
